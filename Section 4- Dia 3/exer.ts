9987189ff883570657a51bb7fa48ffd25fa07ef4
Marlon Martins <marlon-martins@outlook.com>
2024-04-24 13:56:23 +0200

    40. Exercicio

---
+++ Section 4- Dia 3/exer.ts
@@ -0,0 +1,26 @@
+// Day 3 - Exercise 1
+
+let pi = 3.14159;
+let tau = pi * 2;
+
+console.log(`${tau} is ${pi} times two.`)
+
+// Day 3 - Exercise 2
+let pie: string;
+
+pie = 'blueberry';
+
+console.log(`I like to eat ${pie}-flavored pie.`)
+
+// Day 3 - Exercise 3
+let isDouglas: boolean;
+isDouglas = true;
+
+console.log(`${isDouglas ? 'oh! Hi Douglas' : 'Who are you?'}`);
+
+// Day 3 - Exercise 4
+
+const sequence: number[] = Array.from(Array(10).keys());
+const animals: string[] = ['apple', 'banana', 'cherry'];
+const stringsAndNumbers: (number | string)[] = [1, 'one', 2, 'two', 3, 'three'];
+const allMyArrays:(number | string)[][] = [sequence,animals,stringsAndNumbers];
---
+++ Section 4- Dia 3/prod/exer.js
@@ -0,0 +1,18 @@
+"use strict";
+// Day 3 - Exercise 1
+let pi = 3.14159;
+let tau = pi * 2;
+console.log(`${tau} is ${pi} times two.`);
+// Day 3 - Exercise 2
+let pie;
+pie = 'blueberry';
+console.log(`I like to eat ${pie}-flavored pie.`);
+// Day 3 - Exercise 3
+let isDouglas;
+isDouglas = true;
+console.log(`${isDouglas ? 'oh! Hi Douglas' : 'Who are you?'}`);
+// Day 3 - Exercise 4
+const sequence = Array.from(Array(10).keys());
+const animals = ['apple', 'banana', 'cherry'];
+const stringsAndNumbers = [1, 'one', 2, 'two', 3, 'three'];
+const allMyArrays = [sequence, animals, stringsAndNumbers];
