6de12e782e18af93c0be1f493ed1bfc8522db0b8
Marlon Martins <marlon-martins@outlook.com>
2024-04-26 15:26:48 +0200

    80. Exer 4

---
+++ Section 8- Dia 7/prod/exer4.js
@@ -0,0 +1,11 @@
+"use strict";
+// Day 7 - Exercise 4
+let multiply;
+let capitalize;
+capitalize = function (value) {
+    return `${value.charAt(0).toUpperCase()}${value.slice(1)}`;
+};
+multiply = function (x, y) {
+    return x * y;
+};
+console.log(capitalize(`nifty ${multiply(5, 10)}`));
