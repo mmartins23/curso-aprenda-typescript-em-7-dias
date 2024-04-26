63acc2b7f1da43128d50ff35566256b36799468d
Marlon Martins <marlon-martins@outlook.com>
2024-04-26 14:49:42 +0200

    76. Exer 1

---
+++ Section 8- Dia 7/prod/exer.js
@@ -0,0 +1,35 @@
+"use strict";
+// Day 7 - Exercise 1
+const persons = [
+    {
+        name: 'Max Mustermann',
+        age: 25,
+        occupation: 'Chimney sweep'
+    },
+    {
+        name: 'Jane Doe',
+        age: 32,
+        role: 'Administrator'
+    },
+    {
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut.'
+    },
+    {
+        name: 'Bruce Willis',
+        age: 64,
+        role: 'World saver.'
+    }
+];
+function logPerson(person) {
+    let additionalInformation;
+    if ("role" in person) {
+        additionalInformation = person.role;
+    }
+    else {
+        additionalInformation = person.occupation;
+    }
+    console.log(` - ${person.name}, ${person.age}, ${additionalInformation}`);
+}
+persons.forEach(logPerson);
